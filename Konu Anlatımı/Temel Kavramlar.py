ee0761c4ed7a800b9521d47bcf1961c8f093a187
MahmutTurkmen <150715109+MahmutTurkmen@users.noreply.github.com>
2025-12-14 15:06:49 +0300

    Add basic Python concepts and examples

---
+++ Konu Anlatımı/Temel Kavramlar.py
@@ -0,0 +1,24 @@
+# Bu bir yorum satırıdır (yorumlar # ile başlar)
+# Ekrana mesaj yazdırma
+print("Hello World!") # Çıktı: Hello World!
+print("Merhaba Python!") # Çıktı: Merhaba Python
+# Kullanıcıdan giriş alma
+isim = input("Adınızı girin: ")
+# Veri tiplerini ekrana yazdırma
+sayi = 42
+pi = 3.14
+dogru_mu = True
+yanlis_mi = False
+
+print("Senin adın: ",isim) # Örnek çıktı: Senin adın: Mahmut
+print("Bir sayı: ",sayi) # Çıktı: 42
+print("Pi sayısı: ",pi) # Çıktı: 3.14
+print("Doğru mu: ",dogru_mu) # Çıktı: True
+print("Yanlış mı: ",yanlis_mi) # Çıktı: False
+
+# type() fonksiyonu ile tip kontrolü
+print("isim değişkeninin tipi: ",type(isim))
+print("sayi değişkeninin tipi: ",type(sayi))
+print("pi değişkeninin tipi: ",type(pi))
+print("dogru_mu değişkeninin tipi: ",type(dogru_mu))
+print("yanlis_mi değişkeninin tipi: ",type(yanlis_mi))
